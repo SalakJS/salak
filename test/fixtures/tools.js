19f73436d44b139c31c4b0dd6aaec6b81cb416da
wengeek <wenwei897684475@gmail.com>
2018-07-26 10:35:14 +0800

    refactor: config combination

--- test/fixtures/tools.js
+++ test/fixtures/tools.js
@@ -3,7 +3,7 @@ const Salak = require('../..')
 
 module.exports = {
   createApp (name, options) {
-    const baseDir = path.join(__dirname, name)
+    const baseDir = path.join(__dirname, 'salak', name)
 
     return new Salak({
       baseDir
